e5c2902be304e8e2e87fc6e97cdf0c3446148c49
gp22 <gp22@tutanota.com>
2017-03-26 19:09:43 +0700

    more work on richieRich

--- algorithms/richieRich/richieRich.js
+++ algorithms/richieRich/richieRich.js
@@ -29,9 +29,10 @@
     function richieRich(length, availableChanges, number) {
       let numberAsArray = number.split('');
       let numberAsArrayMap = Array(length+1).join('0').split('');
+      let changesRequired = 0;
+      let extraChanges = 0;
 
-      // go through the string comparing numbers from both ends
-      // and add the appropriate values to numberAsArrayMap
+      // add the appropriate values to numberAsArrayMap
       // 1 if one of the numbers is 9
       // 2 if neither of the numbers is 9
       // -1 if both of the numbers are 9
@@ -39,6 +40,7 @@
       for (let i = 0; i < Math.floor(length / 2); i++) {
         // if they're not equal
         if (numberAsArray[i] !== numberAsArray[length-(i+1)]) {
+          changesRequired++;
           let first = numberAsArray[i];
           let second = numberAsArray[length-(i+1)];
           // if at least one of them is 9, change the corresponding value in
@@ -62,50 +64,30 @@
         }
       }
 
+      // figure out if the changes are possible, and how many extra
+      // changes we have, if any
+      if (changesRequired > availableChanges) {
+        return -1;
+      } else {
+        extraChanges = availableChanges - changesRequired;
+      }
 
+      // now go and make the changes
+      for (let i = 0; i < Math.floor(length / 2); i++) {
+        // only make changes if both numbers are not 9
+        if (numberAsArrayMap[i] != -1) {
+          
+        }
+      }
 
-      //   if (numberAsArray[i] !== numberAsArray[length-(i+1)]) {
-      //     let first = numberAsArray[i];
-      //     let second = numberAsArray[length-(i+1)];
-      //     // if neither number is 9, add them to valuesThatAreNotNine
-      //     if (first !== '9' && second !== '9') {
-      //       valuesThatAreNotNine[i] = 1;
-      //       valuesThatAreNotNine[length-(i+1)] = 1;
-      //     } else {
-      //       valuesThatAreNotNine[i] = 2;
-      //       valuesThatAreNotNine[length-(i+1)] = 2;
-      //     }
-      //     if (availableChanges > 0) {
-      //     // change the smaller to equal the larger
-      //     (first > second) ?
-      //       numberAsArray[length-(i+1)] = first : numberAsArray[i] = second;
-      //       availableChanges--;
-      //     } else {
-      //       return -1;
-      //     }
-      //   }
-      // }
-
-      // if (availableChanges > 0) {
-      //   // if there are changes left over, go back and change the pairs to 9
-      //   for (let i = 0; i < Math.floor(length / 2); i++) {
-      //     if (valuesThatAreNotNine[i] === 1) {
-      //       numberAsArray[i] = '9';
-      //       numberAsArray[length-(i+1)] = '9';
-      //       availableChanges--;
-      //     } else if (valuesThatAreNotNine[i] === 2 && availableChanges >= 2) {
-      //       numberAsArray[i] = '9';
-      //       numberAsArray[length-(i+1)] = '9';
-      //     }
-      //   }
-      // }
-
-      console.log(numberAsArrayMap);
+      // console.log(changesRequired);
+      // console.log(extraChanges);
+      // console.log(numberAsArrayMap);
       // console.log(availableChanges);
-      // return numberAsArray.join('');
+      return numberAsArray.join('');
 
     }
     // richieRich(n, k, number);
-    console.log(richieRich(7, 5, '0921292'));
+    console.log(richieRich(7, 2, '0921282'));
 
 // }
