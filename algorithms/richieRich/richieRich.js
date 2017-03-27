2249bade49d03c385963ce74b1a4748b764985c0
gp22 <gp22@tutanota.com>
2017-03-27 22:14:17 +0700

    add status checking logic to richieRich

--- algorithms/richieRich/richieRich.js
+++ algorithms/richieRich/richieRich.js
@@ -28,7 +28,7 @@
 
     function richieRich(length, availableChanges, number) {
       let numberAsArray = number.split('');
-      let numberAsArrayMap = Array(length+1).join('0').split('');
+      let numberAsArrayMap = Array(length);
       let changesRequired = 0;
       let extraChanges = 0;
 
@@ -54,13 +54,15 @@
             numberAsArrayMap[i] = 2;
             numberAsArrayMap[length-(i+1)] = 2;
           }
-        } else {
+        } else if (numberAsArray[i] === '9') {
           // the numbers are both the same, if they're both 9, change the
           // corresponding values in numberAsArrayMap to -1
-          if (numberAsArray[i] === '9') {
-            numberAsArrayMap[i] = -1;
-            numberAsArrayMap[length-(i+1)] = -1;
-          }
+          numberAsArrayMap[i] = -1;
+          numberAsArrayMap[length-(i+1)] = -1;
+        } else {
+          // otherwise, set them both to 0
+          numberAsArrayMap[i] = 0;
+          numberAsArrayMap[length-(i+1)] = 0;
         }
       }
 
@@ -74,20 +76,34 @@
 
       // now go and make the changes
       for (let i = 0; i < Math.floor(length / 2); i++) {
-        // only make changes if both numbers are not 9
-        if (numberAsArrayMap[i] != -1) {
-          
+        let status = numberAsArrayMap[i];
+        // change both numbers to 9 if neither is 9 and there are
+        // changes changes available
+        if ((status === 2 || status === 0) && extraChanges > 0) {
+          numberAsArray[i] = '9';
+          numberAsArray[length-(i+1)] = '9';
+          extraChanges--;
+        } else if (status === 2) {
+          let first = numberAsArray[i];
+          let second = numberAsArray[length-(i+1)];
+          if (first > second) {
+            numberAsArray[i] = first;
+            numberAsArray[length-(i+1)] = first;
+          } else {
+            numberAsArray[i] = second;
+            numberAsArray[length-(i+1)] = second;
+          }
         }
       }
 
       // console.log(changesRequired);
-      // console.log(extraChanges);
-      // console.log(numberAsArrayMap);
+      console.log(extraChanges);
+      console.log(numberAsArrayMap);
       // console.log(availableChanges);
       return numberAsArray.join('');
 
     }
     // richieRich(n, k, number);
-    console.log(richieRich(7, 2, '0921282'));
+    console.log(richieRich(7, 5, '0721282'));
 
 // }
