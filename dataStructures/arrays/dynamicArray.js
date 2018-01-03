37f93a45c0c5ae6dbf64f2ee5521f963ddd6f274
gp22 <gp22@tutanota.com>
2018-01-02 20:57:26 -0500

    more work on solution

--- dataStructures/arrays/dynamicArray.js
+++ dataStructures/arrays/dynamicArray.js
@@ -1,21 +1,22 @@
 function processData(input) {
   let lastAnswer = 0;
   const splitInput = input.split('\n');
-  const N = splitInput[0].split(' ')[0];
-  const Q = splitInput[0].split(' ')[1];
+  const N = Number(splitInput[0].split(' ')[0]);
+  const Q = Number(splitInput[0].split(' ')[1]);
   const seqList = splitInput.slice(1);
-  const seq = [
-    [], []
-  ];
+  const seq = Array(N).fill(0).map(i => i = []);
   const findSeq = (x) => (x^lastAnswer) % N;
 
-  seqList.forEach(query => {
+  seqList.forEach((query, i) => {
     const type = Number(query.split(' ')[0]);
     const x = Number(query.split(' ')[1]);
     const y = Number(query.split(' ')[2]);
 
     if (type === 1) {
+      console.log(`query ${i}: append ${y} to seq ${findSeq(x)}`);
       seq[findSeq(x)].push(y);
+      console.log('s0: ', seq[0]);
+      console.log('s1: ', seq[1]);
     } else {
       const tempSeq = seq[findSeq(x)];
       const value = y % seq.length;
